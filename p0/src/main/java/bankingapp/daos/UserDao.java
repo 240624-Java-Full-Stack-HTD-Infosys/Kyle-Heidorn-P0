1dec9c2148785a09baf7126d052f1661448e17d7
Kyle Heidorn <kyle841@revature.net>
2024-07-03 18:14:10 -0400

    user dao

--- p0/src/main/java/bankingapp/daos/UserDao.java
+++ p0/src/main/java/bankingapp/daos/UserDao.java
@@ -1,4 +1,73 @@
 package bankingapp.daos;
 
+import bankingapp.models.User;
+import bankingapp.utils.ConnectionUtils;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserDao {
+
+    private Connection connection;
+
+    public UserDao(Connection connection) throws SQLException, IOException {
+        this.connection = ConnectionUtils.getConnection();
+    }
+
+    public void create(User user) throws SQLException {
+        String sql = "INSERT INTO users (username, password, email, firstName, lastName) VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, user.getUsername());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setString(4, user.getFirstName());
+            pstmt.setString(5, user.getLastName());
+            pstmt.executeUpdate();
+    }
+
+    public User read(int id) throws SQLException{
+        String sql = "SELECT * FROM users WHERE username = ?";
+        PreparedStatement pstmt = connection.prepareStatement(sql);
+        pstmt.setInt(1, id);
+        ResultSet results = pstmt.executeQuery();
+
+        User user = new User();
+        if(results.next()) {
+            user.setId(results.getInt("id"));
+            user.setUsername(results.getString("username"));
+            user.setPassword(results.getString("password"));
+            user.setEmail(results.getString("email"));
+            user.setFirstName(results.getString("firstName"));
+            user.setLastName(results.getString("lastName"));
+        }else{
+            return null;
+        }
+
+        return user;
+    }
+
+    public void update(User user) throws SQLException {
+        String sql = "UPDATE users SET username = ?, password = ?, email = ?, firstName = ?, lastName = ? WHERE id = ?";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, user.getUsername());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setString(4, user.getFirstName());
+            pstmt.setString(5, user.getLastName());
+            pstmt.setInt(6, user.getId());
+            pstmt.executeUpdate();
+        }
+    }
+
+    public void delete(User user) throws SQLException {
+        String sql = "DELETE FROM users WHERE id = ?";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setInt(1, user.getId());
+            pstmt.execute();
+        }
+    }
+
 }
--- p0/src/main/java/bankingapp/models/User.java
+++ p0/src/main/java/bankingapp/models/User.java
@@ -59,5 +59,6 @@ public class User {
         this.password = password;
     }
 
+
 }
 
